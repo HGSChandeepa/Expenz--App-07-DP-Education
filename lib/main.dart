9388db4bcdbba6cb03484663675694294145570a
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-04-03 21:22:03 +0530

    Onboarding Screens

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:expenz/screens/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnBoardingScreen(),
+    );
+  }
+}
